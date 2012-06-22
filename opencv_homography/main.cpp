23f800f89c4637ec0a32d0a07df4d3d2446e34fb
Nobuyuki Matsui <nobuyuki.matsui@gmail.com>
2012-06-23 07:05:19 +0900

    Initial Commit

---
+++ opencv_homography/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  opencv_homography
+//
+//  Created by cube on 12/06/23.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#include <iostream>
+
+int main (int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
